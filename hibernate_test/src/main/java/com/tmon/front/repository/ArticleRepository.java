82de81842bb4e4c2be9418ea125f579d82c77ad6
jekalmin <jekalmin@tmon.co.kr>
2014-10-01 14:12:29 +0900

    hibernate_test modified

--- hibernate_test/src/main/java/com/tmon/front/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tmon.front.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tmon.front.Article;
-
-public interface ArticleRepository extends CrudRepository<Article, Integer>{
-
-}
--- hibernate_test/src/test/java/com/tmon/front/HibernateTest.java
+++ hibernate_test/src/test/java/com/tmon/front/HibernateTest.java
@@ -1,72 +1,91 @@
 package com.tmon.front;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.tmon.front.domain.Article;
+import com.tmon.front.domain.Customer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:/spring/application-config.xml"})
 public class HibernateTest {
 
-	@Autowired
 	SessionFactory factory;
 	Session session;
 	
-//	@Before
-//	public void before(){
-//		Configuration configuration = new Configuration().configure("hibernate/hibernate.cfg.xml");
-//		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-//		factory = configuration.buildSessionFactory(builder.build());
-//		session = factory.openSession();
-//	}
-//	
-//	@After
-//	public void after(){
-//		session.close();
-//		factory.close();
-//	}
-	
 	@Before
 	public void before(){
+		Configuration configuration = new Configuration().configure("hibernate/hibernate.cfg.xml");
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+		factory = configuration.buildSessionFactory(builder.build());
 		session = factory.openSession();
 	}
-
+	
+	@After
+	public void after(){
+		session.close();
+		factory.close();
+	}
+	
+	@Test
+	public void test() throws Exception{
+		Article article = new Article(1,"1","1");
+		
+		Field field = Article.class.getDeclaredField("articleNo");
+		System.out.println(field);
+//		field.setAccessible(true);
+		System.out.println(field.get(article));
+		
+	}
+	
 	@Test
 	public void insert(){
-		System.out.println("test");
 		Customer c1 = new Customer();
 		c1.setId("jekalmin");
 		c1.setName("min");
 		c1.setPassword("min");
 		session.save(c1);
 		
+	}
+	
+	@Test
+	public void insertArticle(){
 		
+		for(int i=0; i < 10; i++){
+			Article article = new Article();
+			article.setMemberSeq(1);
+			article.setTitle("제목");
+			article.setContent("내용");
+			session.save(article);
+		}
 		
-		
+		list();
 	}
 	
 	@Test
 	public void detail(){
-		Customer c1 = (Customer)session.get(Customer.class, 2);
-		Customer c2 = (Customer)session.load(Customer.class, 3);
+		Customer c1 = (Customer)session.get(Customer.class, 1);
 		System.out.println(c1);
-		System.out.println(c2);
 	}
 	
 	@Test
 	public void list(){
-		Query query = session.createQuery("from Customer");
 //		SQLQuery query = session.createSQLQuery("select * from Customer");
-		List<Customer> customers = query.list();
-		for(Customer customer : customers){
+		Query query = session.createQuery("from Article");
+		List customers = query.list();
+		for(Object customer : customers){
 			System.out.println(customer);
 		}
 	}
