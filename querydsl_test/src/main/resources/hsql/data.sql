115bf937377ff9878d0a3d7130656a734e71c1d6
jekalmin <jekalmin@tmon.co.kr>
2014-10-01 15:13:20 +0900

    querydsl_test created

---
+++ querydsl_test/src/main/resources/hsql/data.sql
@@ -0,0 +1,7 @@
+insert into Article(articleNo, memberSeq, title, content) values(1, 1, '가나다1', 'ㅋㅋㅋ1');
+insert into Article(articleNo, memberSeq, title, content) values(2, 2, '가나다2', 'ㅋㅋㅋ2');
+insert into Article(articleNo, memberSeq, title, content) values(3, 3, '가나다3', 'ㅋㅋㅋ3');
+insert into Article(articleNo, memberSeq, title, content) values(4, 4, '가나다4', 'ㅋㅋㅋ4');
+insert into Article(articleNo, memberSeq, title, content) values(5, 5, '가나다5', 'ㅋㅋㅋ5');
+
+
---
+++ querydsl_test/src/test/java/com/tmon/front/JpaTest.java
@@ -0,0 +1,52 @@
+package com.tmon.front;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.tmon.front.domain.Article;
+import com.tmon.front.repository.ArticleRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:spring/application-config.xml")
+public class JpaTest {
+	
+	@Autowired
+	ArticleRepository repository;
+
+	@Test
+	public void insert(){
+		System.out.println("test");
+		repository.save(new Article(6, 1, "제목1", "내용1"));
+		list();
+	}
+	
+	@Test
+	public void detail(){
+		Article article = repository.findOne(2);
+		System.out.println(article);
+	}
+	
+	@Test
+	public void list(){
+		Iterable<Article> iter = repository.findAll();
+		for(Article article : iter){
+			System.out.println(article);	
+		}
+	}
+	
+	@Test
+	public void update(){
+		list();
+		repository.save(new Article(1,1, "변경된제목", "변경된내용"));
+		list();
+	}
+	
+	@Test
+	public void delete(){
+		repository.delete(2);
+		list();
+	}
+}
